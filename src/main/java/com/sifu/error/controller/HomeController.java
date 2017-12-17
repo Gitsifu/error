849ad5c34943b9e79ab47b9c16f9dd6497acbab7
sifu <2587107273@qq.com>
2017-12-17 19:11:28 +0800

    添加返回json格式异常数据

--- src/main/java/com/sifu/error/controller/HomeController.java
+++ src/main/java/com/sifu/error/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.sifu.error.controller;
 
 import com.sifu.error.exception.LogicException;
+import com.sifu.error.exception.MyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,5 +21,11 @@ public class HomeController {
     @RequestMapping("/hi")
     public String test()throws LogicException{
         throw new LogicException("发生逻辑错误");
+    }
+
+
+    @RequestMapping("/json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
     }
 }
---
+++ src/main/java/com/sifu/error/exception/MyException.java
@@ -0,0 +1,12 @@
+package com.sifu.error.exception;
+
+/**
+ * @author sifu
+ * @version 1.0
+ * @date 2017/12/17
+ */
+public class MyException extends RuntimeException {
+    public MyException(String message){
+        super(message);
+    }
+}
